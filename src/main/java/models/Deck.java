5bf0f66be8be87d2ef6c624c4f3d7148a2889e77
kolasanm <kolasanm@oregonstate.edu>
2017-11-26 01:01:30 -0800

    refactored code to include OOdesign and prepared the code for sprint 4

--- src/main/java/models/Deck.java
+++ src/main/java/models/Deck.java
@@ -1,31 +1,35 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
 
 public class Deck {
 
-    public java.util.List<Card> cards = new ArrayList<>(); // 'cards' is the array of cards
+    public java.util.List<Card> cards = new ArrayList<>();
 
     public Deck() {
-
-        //builds the deck
-        for (int i = 2; i < 15; i++) {
-            cards.add(new Card(i, Suit.Clubs));
-            cards.add(new Card(i, Suit.Hearts));
-            cards.add(new Card(i, Suit.Diamonds));
-            cards.add(new Card(i, Suit.Spades));
+        for(int i = 2; i < 15; i++){
+            cards.add(new Card(i,Suit.Clubs));
+            cards.add(new Card(i,Suit.Hearts));
+            cards.add(new Card(i,Suit.Diamonds));
+            cards.add(new Card(i,Suit.Spades));
         }
+    }
 
-        //shuffles the deck
-        for (int i = 0; i < 51; i++) {
-            int random = i + (int) (Math.random() * (52 - i)); //Randomly generates a number from 1 - 51
-            Card temp = cards.get(random); //Initialize a temp variable that stores the position of a card
-            cards.set(random, cards.get(i)); //Switch the card
-            cards.set(i, temp); //Store the card in index i in temp
-        } //Run through 52 iterations
+    public void shuffle() {
+        long seed = System.nanoTime();
+        Collections.shuffle(cards, new Random(seed));
+    }
 
-        for (int i = 0; i < 52; i++) {
-            System.out.println(cards.get(i)); //Print out the deck to test the shuffle function
+    public ArrayList<Card> dealFour() {
+        ArrayList<Card> four = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            if (cards.size() > 0) {
+                four.add(cards.get(i));
+                cards.remove(i);
+            }
         }
+        return four;
     }
 }
